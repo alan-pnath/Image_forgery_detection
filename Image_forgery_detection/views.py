f3d35f1c479761474b0a992d3ea20a6f89013f9b
ALAN P NATH <68515757+alan-pnath@users.noreply.github.com>
2023-04-16 12:09:36 +0530

    coded

--- Image_forgery_detection/views.py
+++ Image_forgery_detection/views.py
@@ -3,54 +3,68 @@ from django.shortcuts import render
 import cv2
 import numpy as np
 from keras.models import load_model
+from PIL import Image, ImageChops, ImageEnhance
+import matplotlib.pyplot as plt
 
+class_names = ['Forged', 'Authentic']
+model=load_model("image-forgery-detection-main/trained_model1.h5")
 
 def home(request):
     if request.method == 'POST':
         uploaded_image = request.FILES['input_image']
+        test_image = prepare_image(uploaded_image)
+        test_image = test_image.reshape(-1, 128, 128, 3)
+        y_pred = model.predict(test_image)
+        y_pred_class = round(y_pred[0][0])
 
+        fig, ax = plt.subplots(1,2,figsize=(15,5)) 
 
-        # Set the BDCT block size
-        block_size = 8
+        #display original image
+        original_image = plt.imread(uploaded_image) 
+        ax[0].axis('off')
+        ax[0].imshow(original_image)
+        ax[0].set_title('Original Image')
 
-        # Load the image and convert it to grayscale
-        img = cv2.imread("uploaded_image")
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #display ELA applied image
+        ax[1].axis('off')
+        ax[1].imshow(convert_to_ela_image(uploaded_image,90)) 
+        ax[1].set_title('ELA Image')
 
-        # Convert the image to float32 data type
-        img = img.astype(np.float32)
-
-        # Compute the BDCT coefficients for each block
-        bdct = np.zeros(img.shape)
-        for i in range(0, img.shape[0], block_size):
-            for j in range(0, img.shape[1], block_size):
-                bdct[i:i+block_size, j:j+block_size] = cv2.dct(
-                    img[i:i+block_size, j:j+block_size])
-
-        # Compute the threshold using the mean and standard deviation of the BDCT coefficients
-        threshold = np.mean(bdct) + np.std(bdct)
-
-        # Apply the threshold to the BDCT coefficients to obtain a binary mask
-        mask = (bdct > threshold).astype(np.uint8)
-
-        # Resize the mask to match the input size of the CNN
-        mask = cv2.resize(mask, (224, 224))
-
-        # Stack the mask together as input to the CNN
-        X = np.expand_dims(mask, axis=-1)
-        X = np.expand_dims(X, axis=0)
-
-        # Load the trained model
-        model = load_model("image-forgery-detection-main/trained_model1.h5")
-
-        # Predict the authenticity of the image
-        prediction = model.predict(X)
-
-        if prediction[0][0] > 0.5:
-            print("The image is authentic.")
+        print(f'Prediction: {class_names[y_pred_class]}')
+        if y_pred<=0.5:
+            print(f'Confidence:  {(1-(y_pred[0][0])) * 100:0.2f}%')
         else:
-            print("The image is forged.")
-
+            print(f'Confidence: {(y_pred[0][0]) * 100:0.2f}%')
+        print('--------------------------------------------------------------------------------------------------------------')
 
 
     return render(request, 'base.html')
+
+def prepare_image(image_path):
+    image_size = (128, 128)
+    return np.array(convert_to_ela_image(image_path, 90).resize(image_size)).flatten() / 255.0         #normalizing the array values obtained from input image
+
+def convert_to_ela_image(path,quality):
+
+    original_image = Image.open(path).convert('RGB')
+
+    #resaving input image at the desired quality
+    resaved_file_name = 'resaved_image.jpg'     #predefined filename for resaved image
+    original_image.save(resaved_file_name,'JPEG',quality=quality)
+    resaved_image = Image.open(resaved_file_name)
+
+    #pixel difference between original and resaved image
+    ela_image = ImageChops.difference(original_image,resaved_image)
+    
+    #scaling factors are calculated from pixel extremas
+    extrema = ela_image.getextrema()
+    max_difference = max([pix[1] for pix in extrema])
+    if max_difference ==0:
+        max_difference = 1
+    scale = 350.0 / max_difference
+    
+    #enhancing elaimage to brighten the pixels
+    ela_image = ImageEnhance.Brightness(ela_image).enhance(scale)
+
+    ela_image.save("ela_image.png")
+    return ela_image
