8c7c4342733e4be2fb22e2d16af5a5b69b78e77b
ALAN P NATH <68515757+alan-pnath@users.noreply.github.com>
2023-05-29 21:16:12 +0530

    new model trained vgg-16

--- Image_forgery_detection/views.py
+++ Image_forgery_detection/views.py
@@ -1,15 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-import cv2
 import numpy as np
 from keras.models import load_model
 from PIL import Image, ImageChops, ImageEnhance
 import matplotlib.pyplot as plt
 import io
 import imghdr
-import requests
-import json
-from sightengine.client import SightengineClient
 
 
 class_names = ['Forged', 'Authentic']
@@ -31,21 +26,6 @@ def home(request):
             
                 if img.format in ['JPEG', 'PNG']:
                     test_image = prepare_image(uploaded_image)
-                    # params = {
-                    # 'models': 'properties,scam',
-                    # api_user: '1641196078',
-                    # api_secret: 'aLanF9RyfpZowysgemsx'
-                    # }
-                    # files = {'media': open('static/predicted/ela_image.png', 'rb')}
-                    # r = requests.post('https://api.sightengine.com/1.0/check.json', files=files, data=params)
-
-                    # output = json.loads(r.text)
-                    # print(output)
-                    
-                    # client = SightengineClient("1641196078", "aLanF9RyfpZowysgemsx")
-                    # output = client.check('type').set_file('static/predicted/resaved_image.jpg')
-                    # print(output)
-                    
                     test_image = test_image.reshape(-1, 128, 128, 3)
                     y_pred = model.predict(test_image)
                     y_pred_class = round(y_pred[0][0]) 
@@ -65,14 +45,12 @@ def home(request):
                     data['error'] = "Invalid Format. upload JPEG/PNG Formats"
                     res = render(request, 'base.html', data)
                     return res
-                    # data = "Invalid Format. upload JPEG/PNG Formats"
-                    # render(request,  {'error':data})
+                   
             else:
                 data['error'] = "please upload an image file"
                 res = render(request, 'base.html', data)
                 return res
-                # data = "please upload an image file"
-                # render(request, 'base.html', {'error':data})  
+              
 
     return render(request, 'base.html')
 
@@ -82,10 +60,6 @@ def prepare_image(image_path):
     image_size = (128, 128)
     return np.array(convert_to_ela_image(image_path, 90).resize(image_size)).flatten() / 255.0         #normalizing the array values obtained from input image
 
-
-# def prepare_img(image_path):
-#     image_size = (128, 128)
-#     return np.array(compute_ela_cv(image_path, 90).resize(image_size)).flatten() / 255.0
 
 
 def convert_to_ela_image(path,quality):
@@ -113,27 +87,8 @@ def convert_to_ela_image(path,quality):
     ela_image.save("static/predicted/ela_image.png")
     ela_image_gray = ela_image.convert('L')
     
-# Save the grayscale image
     ela_image_gray.save("static/predicted/ela_image_gray.png")
 
-# Save the grayscale map image
-    
     return ela_image
 
 
-# def compute_ela_cv(path, quality):
-#     temp_filename = 'temp_file_name.jpg'
-#     SCALE = 15
-#     orig_img = cv2.imread(path)
-#     orig_img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2RGB)
-    
-#     cv2.imwrite(temp_filename, orig_img, [cv2.IMWRITE_JPEG_QUALITY, quality])
-
-#     # read compressed image
-#     compressed_img = cv2.imread(temp_filename)
-
-#     # get absolute difference between img1 and img2 and multiply by scale
-#     diff = SCALE * cv2.absdiff(orig_img, compressed_img)
-#     diff.save("static/predicted/elacv/temp.png")
-    
-#     return diff
